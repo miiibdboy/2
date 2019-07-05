d01c6f6d64dcd49877c6732108c08af930930a03
miiibdboy <50560477+miiibdboy@users.noreply.github.com>
2019-07-05 22:47:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['pehelp','Type mhelp'];
+    var setGame = ['pehelp','Type pehelp'];
     var i = -1;
     var j = 0;
     setInterval(function (){
